810612a1217d1f7d62ae21f0f0a8238fe1202fb4
saikumar divvela <saikumar.divvela@gmail.com>
2016-12-14 22:11:59 +0900

    updated contents

--- python/class_example.py
+++ python/class_example.py
@@ -1,19 +1,21 @@
 import sys
 
 class person(object):
-    def __init__(self):
-        print ("executing default constuctor")
     def __init__(self,name,age):
         print("executing constructor with arguments")
         self.name = name
         self.age = age
 
+    def __init__(self):
+        print ("executing default constuctor")
+
+
     def sayHello(self):
         #print ("Hello ", (self.name))
-        sys.stdout.write ("Hello \n") 
+        sys.stdout.write ("Hello \n")
 
 
 p1 = person("saikumar",34)
-#p2 = person()
+p2 = person()
 p1.sayHello()
-#p2.sayHello()
+p2.sayHello()
