be2025ffc88d34f15432c6a7d5979546beb1b487
saikumar divvela <saikumar.divvela@gmail.com>
2016-12-23 18:11:40 +0900

    adding doc

--- python/jenkin/TestURL.py
+++
@@ -1,4 +0,0 @@
-import urllib.parse
-print ("hello");
-scheme, netloc = urlsplit("http://168.219.209.58")[0:2]
-print(urlunsplit((scheme, netloc, '/'.join(['build', 'devel:2.3:OSLM:Mobile', '_result']), query, '')))
--- python/stk_cal.py
+++ python/stk_cal.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-print 'hello'
+import csv
 
 '''
 n1 =  156.5
@@ -23,18 +23,32 @@ for x in range(1,2):
     print "#Total %s #Noshares %s #Start price %s #Avgprice %s %.2f %.2f #currentprice %s"%(t_amount,t_shares,p1,avg_price,(avg_price-p1)*100/p1,(c_price-avg_price)*100/avg_price,c_price)
 '''
 
-n1 = 207
-p1 = 282.49
-x1 = p1 * n1
-
-avg_p = 250
-x2    = 32000
+stks = {"YESBAN":{"tgt":1100,"max_inv":100000},"HAVIND":{"tgt":350,"max_inv":100000},"BAJFI":{"tgt":800,"max_inv":100000},"DEWHOU":{"tgt":250,"max_inv":100000},"CANHOM":  {"tgt":1600,"max_inv":60000},"INTDES":{"tgt":150,"max_inv":60000},"MANAFI":{"tgt":60,"max_inv":60000},"GRANUL":{"tgt":100,"max_inv":60000}}
 
 
-n2 =  ((x1 + x2)/avg_p) - n1
-p2 =  x2/n2
+data = {}
+with open("/home/sai/Downloads/8500610131_PortFolioEqt.csv","rb") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if row['Stock Symbol'] in stks.keys():
+            data[row['Stock Symbol']] = {"qty":row['Qty'],"avg_price":row['Average Cost Price'],"curr_price":row["Current Market Price"]}
 
-print "%s %s %s"%(n1,int(p1),int(x1))
-print "%s %s %s avg: %s"%(round(n2),int(p2),int(x2),int(avg_p))
+x2 = 15000
+print "inv: %s"%(x2)
+for stk in stks.keys():
+     n1 = float(data[stk]["qty"])
+     p1 = float(data[stk]["avg_price"])
+     x1 = p1 * n1
+    
+     curr_price = float(data[stk]["curr_price"])
 
+     n2 = x2 / curr_price    
+     new_price = (x1 + x2 ) / (n1+n2)   
+    
+     print stk   
+     print "%s %s %s curr:%s tgt:%s"%(n1,int(p1),int(x1),int(curr_price),stks[stk]["tgt"])
+     print "%s %s %s max_inv:%s remaining:%s"%(round(n2),int(new_price),int(x1+x2),int(stks[stk]["max_inv"]),int(stks[stk]["max_inv"]-x1-x2))
+     n2 = (stks[stk]["max_inv"] - x1) / curr_price
+     new_price = stks[stk]["max_inv"] / (n1+n2)      
 
+     print "%s %s %s max_inv:%s remaining:%s"%(round(n2),int(new_price),int(stks[stk]["max_inv"]),int(stks[stk]["max_inv"]),0)
