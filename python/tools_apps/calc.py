8b65195eca3ff6f1f794a5b909247a1b810dcc48
saikumar divvela <sai.divvela@samsung.com>
2017-01-23 19:42:27 +0900

    updated files

--- python/tools_apps/calc.py
+++ python/tools_apps/calc.py
@@ -50,37 +50,42 @@ def rateof_return(p,m,time,freq):
 The mathematical formula for calculating EMIs is: EMI = [P x R x (1+R)^N]/[(1+R)^N-1], where P stands for the loan amount or principal, R is the interest rate per month [if the interest rate per annum is 11%, then the rate of interest will be 11/(12 x 100)], and N is the number of monthly instalments.
 '''
 def emi(p,i,n):
-    print "EMI: Principle: %s Interest:%s HowManyMonths: %s"%(p,i,n)
+    print "Principle: %s Interest:%s HowManyMonths: %s"%(p,i,n)
 
     r = i/(12*100)
-    emi = p * r * math.pow( 1+r , n)  / (math.pow( 1+r , n) -1)
-    print "EMI: ",math.ceil(emi)," Total interest: ", math.ceil((emi* n - p))
+    emi = int(p * r * math.pow( 1+r , n)  / (math.pow( 1+r , n) -1))
+    print "EMI: ",emi," Total interest: ", emi* n - p
 
     la = p
-    count =1;
+    count =0;
     print '%6s%10s%10s%10s'%('Month','Int','Pri','Bal')
     yi = 0
     yp = 0
-    while la > emi :
-        mi = la * r
-        mp = emi - mi
+    var = 0
+    emi1 = emi
+    while la > 0 :
+        count = count +1
+        mi = int(la * r)
+        mp = emi  - mi
         la = la - mp
-        #print '%6s%10s%10s%10s'%(count,math.ceil(mi),math.ceil(mp),math.ceil(la))
+        #print '%6s%10s%10s%10s%10s'%(count,int(mi),int(mp),int(la),int(emi))
         yi = yi + mi
         yp = yp + mp
         if count%12 == 0:
-            print '%6s%10s%10s%10s'%(count/12,int(math.ceil(yi)),int(math.ceil(yp)),int(math.ceil(la)))
+            print '%6s%10s%10s%10s%10s'%(int(count/12),int(yi),int(yp),int(la),int(emi))
             yi = 0
             yp = 0
+            increment = emi1 * (2/100)*(count/12)
+            emi  = emi1 + increment
 
-        count = count +1
+
 
 if __name__ == '__main__':
-    rd_calc(28000,84,18,12)
-    #fd_calc(15000,24,5,3)
+    rd_calc(10000,24,12,12)
+    fd_calc(10000,12,12,12)
     #rateof_return(1000,4000,6,12)
     #print math.ceil(math.pow(4,1/6))
-    #emi(2800000,10.5,240)
+    emi(280000,10.5,240)
 
 
 '''
--- python/tools_apps/dir_crawler.py
+++ python/tools_apps/dir_crawler.py
@@ -19,24 +19,39 @@ def getfiles(path,extensions=[]):
                 filelist.setdefault(ext, []).append(os.path.join(dirpath,fn))
     return filelist
 
+def getdescription(fn):
+    desc=""
+    with open(fn,'r') as f:
+        for line in f.readlines():
+            if (line.strip()).startswith("###"):
+                desc += (line.strip()).replace("###","")+". "
+    return desc
+
+
 def genhtml(filelist):
     print filelist
     o_html = "<html><head></head><body><font size='3'>"
     for key in filelist.keys():
         for fn in  filelist[key]:
-            o_html = o_html+ "<p><a type='text/plain' target='_blank' href='file:///"+fn+"'>"+os.path.basename(fn)+"</a></p>"
+            desc = getdescription(fn)
+            o_html = o_html+ "<p><a type='text/plain' target='_blank' href='file:///"+fn+"'>"+os.path.basename(fn)+"</a> ("+fn +")</p>"
+            o_html = o_html+"<h5>"+desc+"</h5>"
     o_html = o_html+"</font></body></html>"
     return o_html
 
 
-path='/home/sai/sss/'
-extensions = ['info']
+path='/home/sai/sss/programs/python'
+extensions = ['py']
 o_file= '/home/sai/sss/test_info.html'
+
+
+print "Search directory %s for files %s"%(path,extensions)
 
 
 filelist = getfiles(path,extensions)
 o_html = genhtml(filelist)
 
+print "writing to %s"%(o_file)
 with open(o_file,'w') as of:
     of.write(o_html)
 
